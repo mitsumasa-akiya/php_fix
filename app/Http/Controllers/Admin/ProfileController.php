bf887bb439cc1884d9fc960ae2329eed9215d977
”33_mitsumasa_akiya” <kh1593ssss@gmail.com>
2020-09-17 08:22:20 +0000

    kadai_fix

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Profile;
+
+class ProfileController extends Controller
+{
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create(Request $request)
+    {
+      $this->validate($request, Profiles::$rules);
+      $profile = new Profiles;
+      $form = $request->all();
+
+      unset($form['_token']);
+     
+      $profile->fill($form);
+      $profile->save();
+      
+      return redirect('admin/news/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    } //
+}
